562cf7cb55c72eeaff3ebb6f9d58c97d38d6eb69
DXNTKEY <purplecs96@gmail.com>
2024-05-04 11:26:35 +0700

    Добавьте файлы проекта.

---
+++ 332/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualBasic;
+
+string Name = "Kirill";
+int Age = 19;
+bool Pet = true;
+double FootSize = 41.5;
+Console.WriteLine("My Name is " + Name);
+Console.WriteLine("My age is " + Age);
+Console.WriteLine("Do I have pet? " + Pet);
+Console.WriteLine("My shoe size is " + FootSize);
